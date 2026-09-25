a6b8dcb29d01cd2eafcdf746b674929241d7afbe
ITSK1MLOT <itskimlot@gmail.com>
2026-09-25 20:35:45 +0900

    CSS 마스킹 호환성과 선택자 안정성 개선

--- src/sites/naver.js
+++ src/sites/naver.js
@@ -3,9 +3,9 @@ SimpleSanitizer.register(
   ({ hostname }) => SimpleSanitizer.hostIs(hostname, "naver.com"),
   ({ addStyle, options }) => {
     addStyle(`
-      div[class^="delivery_address"],
-      div[class^="DeliveryContent_"],
-      div[class^="View_view-value__"],
+      div[class*="delivery_address"],
+      div[class*="DeliveryContent_"],
+      div[class*="View_view-value__"],
       #loc-main-section-root {
         display: none !important;
         content-visibility: hidden !important;
@@ -14,7 +14,7 @@ SimpleSanitizer.register(
 
     if (options.hideName) {
       addStyle(`
-        [class^="_name-user_"] {
+        [class*="_name-user_"] {
           display: none !important;
           content-visibility: hidden !important;
         }
